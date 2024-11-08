add708a0f41b235580859e658a20cc5eae4e948d
Nabila Rahman Faija <166171668+NabilaRahmann@users.noreply.github.com>
2024-11-08 20:29:54 +0600

    Add files via upload

---
+++ students-feedback-main/students-feedback-main/README.md
@@ -0,0 +1,26 @@
+# 👨‍🎓📝 Student Feedback Portal
+
+A student feedback portal built using PHP, SQL and CSS.
+
+## ⭐ Features
+
+- The students can go to the feedback form by clicking on Student feedback button on the slide an fill out the form with the information.
+- The staff with admin privileges can login using the admin button on the main page.
+- After successful login, they are taken to the feedback data page. They can see all responses there in table form.
+- A logout button is provided to avoid anyone else accessing the responses.
+
+## ⭐ Used technologies:
+- Html
+- CSS
+- Bootstrap
+- PHP
+- SQL
+- Fontawsome
+
+  Screen Short Full Website
+
+  ![Screenshot 2024-05-22 233310](https://github.com/Monishachy09/students-feedback/assets/86668735/f4309aea-bde2-4a2b-9716-5dfa43f2de52)
+![Screenshot 2024-05-22 234114](https://github.com/Monishachy09/students-feedback/assets/86668735/c64d3570-7b1c-423c-b41d-537f805639b2)
+![Screenshot 2024-05-18 230335](https://github.com/Monishachy09/students-feedback/assets/86668735/a9887b42-c490-4bff-b3cc-05dffa5a21c8)
+![Screenshot 2024-05-18 230400](https://github.com/Monishachy09/students-feedback/assets/86668735/e10999ae-a39a-412a-a72d-6a467ef5124d)
+![Screenshot 2024-05-18 230422](https://github.com/Monishachy09/students-feedback/assets/86668735/52922f72-918b-435e-950c-dba4cede2e37)
---
+++ students-feedback-main/students-feedback-main/index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Student Feedback</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="content-Type" content="text/html; charset=utf-8" />
+    <script type="application/x-javascript">
+      addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+      function hideURLbar(){ window.scrollTo(0,1); }
+    </script>
+    <style>
+        .button-01 ul li{
+            background:none !important;
+            border: 1px solid #fff !important;
+        }
+        .slider .carousel-inner .carousel-item::before{
+            /* background: none !important; */
+            background: rgba(0, 0, 0, 0.5) !important;
+        }
+        img{
+            width:100% !important;
+        }
+    </style>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <link rel="stylesheet" href="css/style.css">
+    
+  </head>
+
+  <body>
+         <!-- ======================header started====================== -->
+
+         <header>
+        <div class="my-nav">
+            <div class="container">
+                <div class="row">
+                    <div class="nav-items">
+                        <div class="menu-toggle"></div>
+                        <div class="logo">
+                            <img src="images/nav-logo.png">
+                        </div>
+                        <div class="menu-items">
+                            <div class="menu">
+                                <ul>
+                                    <li><a href="#">Home</a></li>
+                                    <li><a href="#">About Us</a></li>
+                                    <li><a href="#">Services</a></li>
+                                    <li><a href="#">Blog</a></li>
+                                    <li><a href="admin.php">Admin</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- ======================slider section started====================== -->
+
+    <section id="carouselExampleFade" class="carousel slide carousel-fade slider" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="images/slider/slider1.jpg" class="d-block" alt="...">
+                <div class="carousel-caption">
+                    <h2>Best Education For Honours</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui perspiciatis, eveniet sequi labore vel itaque adipisci odio necessitatibus voluptatibus saepe, impedit enim unde velit amet rem, suscipit corrupti vero ad.</p>
+                    <div class="button-01">
+                        <ul>
+                        <li ><a  href="review.php">Student Feedback</a></li>
+                    
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+            <img src="images/slider/slider2.jpg" class="d-block" alt="...">
+            <div class="carousel-caption">
+                <h2>Best Education For Honours</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui perspiciatis, eveniet sequi labore vel itaque adipisci odio necessitatibus voluptatibus saepe, impedit enim unde velit amet rem, suscipit corrupti vero ad.</p>
+                <div class="button-01">
+                    <ul>
+                    <li><a href="review.php">Student Feedback</a></li>
+                    </ul>
+                </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+            <img src="images/slider/slider3.jpg" class="d-block" alt="...">
+            <div class="carousel-caption">
+                <h2>Best Education For Honours</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui perspiciatis, eveniet sequi labore vel itaque adipisci odio necessitatibus voluptatibus saepe, impedit enim unde velit amet rem, suscipit corrupti vero ad.</p>
+                <div class="button-01">
+                    <ul>
+                    <li><a href="review.php">Student Feedback</a></li>
+                    </ul>
+                </div>
+            </div>
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+    </section>
+  </body>
+  <script src="bootstrap-js/jquery-3.2.1.min.js"></script>
+<script src="bootstrap-js/popper.min.js"></script>
+<script src="bootstrap-js/bootstrap.min.js"></script>
+<script src="bootstrap-js/owl.carousel.min.js"></script>
+<script src="bootstrap-js/owl.carousel.js"></script>
+<script src="bootstrap-js/script.js"></script>
+</html>
